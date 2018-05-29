00229fc45b911e56ee790b6241dcb5d93b0b1821
Kostya Pechenkin <kostya-kap@mail.ru>
2018-05-29 12:44:51 +0300

    complete

---
+++ ClassLibrary1/scroll.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    class scroll
+    {
+        public int scrollID { get; set; }
+        public string scrollName { get; set; }
+        public double scrollStrength { get; set; }
+
+        public scroll (int scrollID, string scrollName, double scrollStrength)
+        {
+            this.scrollID = scrollID;
+            this.scrollName = scrollName;
+            this.scrollStrength = scrollStrength;
+        }
+
+        public virtual void info()
+        {
+            Console.WriteLine("Код номенклатуры: {0}\nНазвание номенклатуры: {1}\nГрадус: {2}", 
+                scrollID, scrollName, scrollStrength);
+        }
+
+        public override string ToString()
+        {
+            return scrollID.ToString() + ": " + scrollName;
+        }
+    }
+}
---
+++ consoleapplication1/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ClassLibrary1;
+
+namespace ConsoleApplication1 {
+    class Program {
+        static void Main(string[] args) {
+            
+            DateTime Start = new DateTime(2018, 1, 20);
+            DateTime End = new DateTime(2018, 2, 20);
+            scroll scroll1 = new scroll(1, "пиво", 10);
+            //product product1 = new product(1, "Балтика", scroll1, 0.5, 70, "Балтика продакшн");
+            //product1.info();
+            List<TemporaryScroll> listscroll = new List<TemporaryScroll>();
+            listscroll.Add(new TemporaryScrollt(2, "Коньяк",40,  Start, End));
+            foreach(TemporaryScroll ff in listscroll)
+            {
+                ff.info();
+                Console.WriteLine();
+            }
+            Console.ReadLine();
+            
+        }
+        
+    }
+}
